85bf8cc4062c4e7900eaff5e61b414b92b2a2ec0
kentavros <assh4884@gmail.com>
2017-08-13 09:55:55 +0300

    first

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include ('libs/config.php');
+include ('libs/function.php');
+
+
+
+
+
+include('template/tmp.php');
+?>
---
+++ libs/config.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Message
+ */
+//Do not use star in select
+define('STAR_USED', 'Do not use * !');
+
+//No object properties found
+define('NO_PROPERTIES', 'No object properties found');
+
+//Error connect to database
+define ('ERROR_CONNECT', 'Error connect to database! Wrong host or user/pass! ');
+
+//Wrong data base name
+define ('ERROR_DB', 'Wrong data base name! ');
+
+//Error query
+define('ERROR_QUERY', 'Error query! ');
+
+//No row
+define('NO_ROW', 'No row - user6');
+
+
+
+/**
+ * for Data Base MySQL
+ */
+define('HOST','localhost');
+
+define('USER_NAME', 'user1');
+
+define('PASS', 'tuser1');
+
+define('DB_NAME', 'user1');
+
+define('TB_NAME', 'MY_TEST');
+
+
+?>
